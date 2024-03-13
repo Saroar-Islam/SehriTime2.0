fdb6e3bbbbe90c2bc0abc8e9a6de31311c4e4953
Saroar Islam <saroar.coding@gmail.com>
2024-03-13 20:46:18 +0600

    file added

---
+++ script.js
@@ -0,0 +1,74 @@
+function convertTo24Hour() {
+    var initialDate = new Date(2024, 2, 11); // Attention: month is zero-based
+    var now = Date.now();
+    var difference = now - initialDate;
+    var millisecondsPerDay = 24 * 60 * 60 * 1000;
+    var daysSince = Math.floor(difference / millisecondsPerDay);
+
+    const daySinceDiv = document.querySelector("#daysince");
+    // daySinceDiv.innerHTML += daysSince;
+
+    let inputBox = document.getElementById("inputBox").value;
+    console.log("inputBox: " + inputBox);
+
+    let main = inputBox + " AM";
+    // console.log("main: " + main);
+
+    let inputTime = main;
+
+    function timeFn(timeString) {
+        let date = new Date(`01/01/2022 ${timeString}`);
+        let formattedTime = date.toLocaleTimeString("en-US", { hour12: false });
+        return formattedTime;
+    }
+
+    let result = timeFn(main);
+
+    let arr = result.split(":");
+    // console.log(result.split(":"));
+
+    var date = new Date();
+    let currentDate = document.querySelector("#currentDate");
+    currentDate.innerHTML = date.getDate();
+    currentDate.innerHTML += " March, ";
+    let ramadan = document.querySelector("#ramadan");
+    ramadan.innerText = daysSince;
+    ramadan.innerText += " Roja";
+
+    let hour = date.setHours(arr[0]);
+    let min = date.setMinutes(arr[1]);
+
+    //lastTime
+    date.setMinutes(arr[1] - daysSince);
+
+    let time0 =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    //minus 1st break
+    date.setMinutes(arr[1] - 20 - daysSince);
+
+    let time1 =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    //minus 2nd break
+    date.setMinutes(arr[1] - 10 - daysSince);
+
+    let time2 =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    let smoke = document.getElementById("smoke");
+    let brush = document.getElementById("brush");
+    let lastTimeDiv = document.getElementById("lastTimeSpan");
+
+    smoke.innerText = " " + time1;
+    brush.innerText = " " + time2;
+    lastTimeDiv.innerText = " " + time0;
+}
+
+function reload() {
+    location.reload();
+}
+
+// var dt = new Date();
+// dt.setMinutes( dt.getMinutes() - 20 );
+// console.log('#####',dt);
